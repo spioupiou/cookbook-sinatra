dfa266b0a68b297fe95b011a9dbc9ac167700427
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-11 15:24:18 +0900

    migration file fix timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_151626) do
+ActiveRecord::Schema.define(version: 2022_01_11_151548) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
